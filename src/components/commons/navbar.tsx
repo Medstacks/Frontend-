16fbda9685997e535e366a9571f6a7559ef5118e
Biokes <106444765+Biokes@users.noreply.github.com>
2025-05-09 23:59:45 +0100

    [main] update: add navbar,update font styles

--- src/components/commons/navbar.tsx
+++ src/components/commons/navbar.tsx
@@ -1,8 +1,10 @@
+import "../../App.css"
 export default function Navbar() { 
     return (
-        <nav className={'flex justify-between items-center px-3 py-2'}>
-            <article className="w-[150px] h-[70px] flex ">
-                <img src="/favicons/favicom.svg" loading="lazy" alt="logo" className={'object-contain w-full h-full'} />
+        <nav className={'flex justify-between items-center px-3 py-2 shadow-md '}>
+            <article className="w-auto h-[30px] flex gap-1 justify-center items-center cursor-pointer">
+                <img src="/favicons/favicon.svg" loading="lazy" alt="logo" className={'object-contain w-full h-full'} />
+                <h6 style={{fontWeight:'600'}}>Agrolink</h6>
             </article>
         </nav>
     )
