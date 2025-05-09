4317ce27867f7c05dd171808d14499f733049cbd
Biokes <106444765+Biokes@users.noreply.github.com>
2025-05-10 00:55:39 +0100

    [auth] update: create auth modal for sign up

--- src/components/commons/navbar.tsx
+++ src/components/commons/navbar.tsx
@@ -1,14 +1,27 @@
 import "../../App.css"
 import { useAuth } from "../../hooks/authContext";
+import { useState } from 'react';
+
 export default function Navbar() { 
-    const {isAuthenticated} = useAuth();
+    const { isAuthenticated, logout } = useAuth();
+    const [isShowingModal, showingModal] = useState<boolean>(false)
+    function handleAuth() {
+        if (isAuthenticated) {
+            logout();
+            return;
+        }
+        showingModal(true)
+    }
     return (
         <nav className={'flex justify-between items-center px-3 py-2 shadow-md '}>
             <article className="w-auto h-[30px] flex gap-1 justify-center items-center cursor-pointer">
                 <img src="/favicons/favicon.svg" loading="lazy" alt="logo" className={'object-contain w-full h-full'} />
                 <h6 style={{fontWeight:'600'}}>Agrolink</h6>
             </article>
-            <button>{isAuthenticated ? "Log out":'Sign in'}</button>
+            <button onClick={handleAuth}>{isAuthenticated ? "Log out" : 'Sign in'}</button>
+            { 
+                isShowingModal && <AuthModal/>
+            }
         </nav>
     )
 }
